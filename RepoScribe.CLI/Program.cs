ce9e757680f3a5e9e692dd7b44319c3244ed6604
Will Morris <willmorris103@gmail.com>
2024-09-26 03:11:29 -0500

    Updates to the Core Lib

---
+++ RepoScribe.CLI/Program.cs
@@ -0,0 +1,36 @@
+﻿using RepoScribe.Core.Utilities;
+using Serilog;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using RepoScribe.CLI.Commands;
+using LibGit2Sharp;
+using PdfSharp.Charting;
+using PdfSharp.Pdf.Content.Objects;
+using RepoScribe.Core.DataModels.Markdown;
+using RepoScribe.Core.DataModels;
+using RepoScribe.Core.FileHandlers;
+using RepoScribe.Core.Helpers;
+using RepoScribe.Core.Services;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace RepoScribe.CLI
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            Logger.Initialize();
+
+            var rootCommand = new RootCommand("RepoScribe - Flatten and document your code repositories");
+
+            rootcommand.addcommand(new FlattenCommand().getcommand());
+            rootcommand.addcommand(new IgnoreCommand().getcommand());
+            rootcommand.addcommand(new RepoCommand().getcommand());
+            rootcommand.addcommand(new ExtractCommand().getcommand());
+
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
---
+++ RepoScribe.Core/DataModels/Markdown/MarkdownContent.cs
@@ -0,0 +1,59 @@
+﻿namespace RepoScribe.Core.DataModels.Markdown
+{
+    public abstract class MarkdownContent
+    {
+        public abstract string ToMarkdown();
+
+        public override string ToString()
+        {
+            return ToMarkdown();
+        }
+
+        public abstract string ApplyTemplate(string template);
+    }
+}
+/*
+ input_type 
+imput_context_source
+public Enum ContextualInputSources {
+    HostFiles,
+    GitHub,
+    Browsing History,
+    Email,
+    Local Files,
+
+}
+
+public enum Domains {
+    Protected,
+    Personal,
+    Interpersonal,
+    Professional,
+    
+
+public Class Cluster
+
+ClusterIndex {
+    public string ClusterId { get; set; }
+    public GUID ClusterIndexId { get; set; }
+    public List<CacheEntry> CacheEntries { get; set; }
+    
+}
+async multi-shot generation based on nearest neighbor search using domaine of previous questions 
+the user Might ask and the context of the current question to generate several responses with diferent 
+seeds and allow for reflection in the monent to keep generating better and better responses untell one is generated
+and the user grades only the last response generated the chain that lwads to the best response is saved and the
+chain that leads to the worst response is discarded the data is used in backpropagation to improve the model imeadiatly
+
+input_context_medium
+
+
+
+Domains are top level contexual data classes
+Domains {
+    public string DomainId { get; set; }
+    public string DomainName { get; set; }
+}
+
+
+ */
