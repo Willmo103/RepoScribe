56a16263448ba591ea2bdd23893bc953e2ab5f33
Will Morris <willmorris103@gmail.com>
2024-09-30 02:20:14 -0500

    More refactoring the cro lib

--- RepoScribe.Core/ContentItems/ContentItem.cs
+++ RepoScribe.Core/ContentItems/ContentItem.cs
@@ -1,20 +1,13 @@
 ﻿using RepoScribe.Core.Abstractions;
+using RepoScribe.Core.DataModels;
 using RepoScribe.Core.DataModels.Enums;
 
 namespace RepoScribe.Core.ContentItems
 {
-    public abstract class ContentItem
+    public abstract class ContentItem : Metadata
     {
-        public int Id { get; set; } // Primary Key for ORM
-        public string Path { get; set; }
-        public string Owner { get; set; }
-        public DateTime LastModified { get; set; }
-        public double SizeMB { get; set; }
-        public string Language { get; set; }
-        public string Content { get; set; }
-        public Domain Domain { get; set; }  // Domain is now implemented
-        public ContextualInputSource ContextSource { get; set; } // Contextual Input Source
-
-        // Navigation properties or additional relationships can be added here
+        public int Id { get; set; }
+        public Domain Domain { get; set; }
+        public ContextualInputSource ContextSource { get; set; }
     }
 }
--- RepoScribe.Core/Database/Entities/ConversationEntity.cs
+++ RepoScribe.Core/Database/Entities/ConversationEntity.cs
@@ -1,12 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace RepoScribe.Core.Database.Entities.UserIO
+namespace RepoScribe.Core.Database.Entities
 {
-    internal class Conversation
+    internal class ConversationEntity
     {
         [Key]
         public Guid ConversationId { get; set; } = Guid.NewGuid();
-        public List<Query> Queries { get; set; } = new List<Query>();
+        public List<QueryEntity> Queries { get; set; } = new List<QueryEntity>();
         public string ConversationName { get; set; } = "";
         public DateTime TimeStamp { get; set; } = DateTime.Now;
         public string Provider { get; set; } = "";
