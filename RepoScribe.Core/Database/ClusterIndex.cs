6484d75c45e7ef7b955ef4aa69c654ef0b856015
Will Morris <willmorris103@gmail.com>
2024-09-30 00:49:48 -0500

    Added Future Data Models UserIO

--- RepoScribe.Core/Database/ClusterIndex.cs
+++ RepoScribe.Core/Database/ClusterIndex.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace RepoScribe.Core.DataModels
+{
+    public class ClusterIndex
+    {
+        public string ClusterId { get; set; }
+        public Guid ClusterIndexId { get; set; }
+        public List<Guid> MemberEmbeddings { get; set; }
+    }
+}
---
+++ RepoScribe.Core/Database/Entities/UserIO/Query.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RepoScribe.Core.Database.Entities.UserIO
+{
+    abstract class Query
+    {
+        [Key]
+        Guid QueryId { get; set; } = Guid.NewGuid();
+        Guid ConversationId { get; set; }
+        String Source { get; set; } = "";
+        String QueryString { get; set; } = "";
+        String Response { get; set; } = "";
+        DateTime TimeStamp { get; set; } = DateTime.Now;
+        Boolean IsResponse { get; set; } = false;
+        Boolean IsQuery { get; set; } = false;
+        Boolean IsUser { get; set; } = false;
+        Boolean IsBot { get; set; } = false;
+        Boolean HasResponse { get; set; } = false;
+        List<Query> Responses { get; set; } = new List<Query>();
+        Boolean IsBestResponse { get; set; } = false;
+        String Provider { get; set; } = "";
+        
+        [ForeignKey("ConversationId")];
+        Conversation Conversation { get; set; }
+    }
+}
