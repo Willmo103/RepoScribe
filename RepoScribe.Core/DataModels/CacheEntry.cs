414af691355d7ba66c608b8de68218143ddb280c
Will Morris <willmorris103@gmail.com>
2024-09-30 00:49:07 -0500

    Added Cache Entry Logic to DataModels

--- RepoScribe.Core/DataModels/CacheEntry.cs
+++ RepoScribe.Core/DataModels/CacheEntry.cs
@@ -1,1 +1,10 @@
-﻿
+﻿namespace RepoScribe.Core.DataModels
+{
+    public class CacheEntry
+    {
+        public Guid CacheEntryId { get; private set; }
+        public List<ClusterIndex> ClusterIndices { get; set; }
+        private Guid KeyWordEmbeddingId { get; }
+        public string[] KeyWords { get; set; }
+    }
+}
